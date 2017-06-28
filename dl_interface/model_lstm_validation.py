543dcfbb36dd93ae57e2c36d9da12869eca820a7
Abhinav Agarwalla <navabhi174@gmail.com>
2017-06-28 16:07:00 +0100

    LSTM Validation Fixes

--- dl_interface/model_lstm_validation.py
+++ dl_interface/model_lstm_validation.py
@@ -100,6 +100,7 @@ class DataIter():
         y = []
         cnn_logits = []
         cnn_y = []
+        self.names = []
         for i in range(LSTMValidConfig.batch_size):
             feat = pickle.load(open(self.images_list[self.iter], 'rb'))
             wsi_name = self.images_list[self.iter].split(os.sep)[-1]
@@ -109,13 +110,17 @@ class DataIter():
             y.append(label)
             cnn_y.append(feat['predictions'].reshape(LSTMValidConfig.PATCH_SIZE, LSTMValidConfig.PATCH_SIZE, 1))
             cnn_logits.append(feat['fc8'].reshape(LSTMValidConfig.PATCH_SIZE, LSTMValidConfig.PATCH_SIZE, LSTMValidConfig.NUM_CLASSES))
+            self.names.append(self.images_list[self.iter].replace('_features.pkl', '_preds.npy').split(os.sep)[-1])
             self.iter += 1
             if self.iter == self.num_samples:
                 self.iter = 0
         return x, y, cnn_y, cnn_logits
 
     def save_predictions(self, probs):
-        print(probs)
+        for i in range(len(self.names)):
+            start = i*LSTMValidConfig.PATCH_SIZE*LSTMValidConfig.PATCH_SIZE
+            end = (i+1)*LSTMValidConfig.PATCH_SIZE*LSTMValidConfig.PATCH_SIZE
+            np.save(LSTMValidConfig.log_dir + os.sep + 'predictions' + os.sep + self.names[i], probs[start:end,])
 
 class LSTMValidation(QObject):
     finished = pyqtSignal()
@@ -227,7 +232,7 @@ class LSTMValidation(QObject):
     @pyqtSlot()
     def train(self):
         # Saver and initialisation
-        print("starting training")
+        print("starting LSTM Model Validation")
         self.initialize()
         self.epoch.emit(0)
         if not os.path.exists(LSTMValidConfig.log_dir):
@@ -271,7 +276,7 @@ class LSTMValidation(QObject):
         model_out_flat = tf.reshape(model_out, shape=(-1, LSTMValidConfig.NUM_CLASSES))
 
         non_tumor_label = tf.subtract(tf.ones((LSTMValidConfig.PATCH_SIZE, LSTMValidConfig.PATCH_SIZE, 1)), labels)
-        combined_label = tf.concat([labels, non_tumor_label], axis=3)
+        combined_label = tf.concat([non_tumor_label, labels], axis=3)
         labels_flat = tf.reshape(combined_label, shape=(-1,LSTMValidConfig.NUM_CLASSES))
 
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model_out_flat,
@@ -285,25 +290,52 @@ class LSTMValidation(QObject):
         cnn_preds_class = tf.argmax(cnn_preds_flat, axis=1)
         labels_class = tf.argmax(labels_flat, axis=1)
 
+        si = int(model_out_flat.get_shape()[0])
+        model_tumor_preds = tf.reshape(tf.slice(model_out_flat, [0, 1], [si, 1]), [si])
+        cnn_tumor_preds = tf.reshape(tf.slice(cnn_preds_flat, [0, 1], [si, 1]), [si])
+
         accuracy_streaming, accuracy_streaming_update = tf.contrib.metrics.streaming_accuracy(model_out_class, labels_class)
         precision_streaming, precision_streaming_update = tf.contrib.metrics.streaming_precision(model_out_class, labels_class)
         recall_streaming, recall_streaming_update = tf.contrib.metrics.streaming_recall(model_out_class, labels_class)
+        precision_thresh, precision_thresh_update = tf.contrib.metrics.streaming_precision_at_thresholds(model_tumor_preds,
+                                                               labels_class,
+                                                                np.arange(0.0,1.0,0.05).astype(np.float32))
+        recall_thresh, recall_thresh_update = tf.contrib.metrics.streaming_recall_at_thresholds(model_tumor_preds,
+                                                                                               labels_class,
+                                                                                               np.arange(0.0, 1.0,
+                                                                                                         0.05).astype(
+                                                                                                   np.float32))
         accuracy_batch, accuracy_batch_update = tf.metrics.accuracy(labels_class, model_out_class)
         precision_batch, precision_batch_update = tf.metrics.precision(labels_class, model_out_class)
         recall_batch, recall_batch_update = tf.metrics.recall(labels_class, model_out_class)
         metrics_op = tf.group(recall_streaming_update, precision_streaming_update, accuracy_streaming_update,
-                              recall_batch_update, precision_batch_update, accuracy_batch_update)
+                              recall_batch_update, precision_batch_update, accuracy_batch_update,
+                              precision_thresh_update, recall_thresh_update)
 
         accuracy_streaming_cnn, accuracy_streaming_cnn_update = tf.contrib.metrics.streaming_accuracy(cnn_preds_class,
                                                                                               labels_class)
         precision_streaming_cnn, precision_streaming_cnn_update = tf.contrib.metrics.streaming_precision(cnn_preds_class,
                                                                                                  labels_class)
         recall_streaming_cnn, recall_streaming_cnn_update = tf.contrib.metrics.streaming_recall(cnn_preds_class, labels_class)
+        precision_thresh_cnn, precision_thresh_cnn_update = tf.contrib.metrics.streaming_precision_at_thresholds(cnn_tumor_preds,
+                                                                                                                labels_class,
+                                                                                                                np.arange(
+                                                                                                                    0.0,
+                                                                                                                    1.0,
+                                                                                                                    0.05).astype(
+                                                                                                                    np.float32))
+        recall_thresh_cnn, recall_thresh_cnn_update = tf.contrib.metrics.streaming_recall_at_thresholds(cnn_tumor_preds,
+                                                                                                       labels_class,
+                                                                                                       np.arange(0.0,
+                                                                                                                 1.0,
+                                                                                                                 0.05).astype(
+                                                                                                           np.float32))
         accuracy_batch_cnn, accuracy_batch_cnn_update = tf.metrics.accuracy(labels_class, cnn_preds_class)
         precision_batch_cnn, precision_batch_cnn_update = tf.metrics.precision(labels_class, cnn_preds_class)
         recall_batch_cnn, recall_batch_cnn_update = tf.metrics.recall(labels_class, cnn_preds_class)
         metrics_op_cnn = tf.group(recall_streaming_cnn_update, precision_streaming_cnn_update, accuracy_streaming_cnn_update,
-                              recall_batch_cnn_update, precision_batch_cnn_update, accuracy_batch_cnn_update)
+                              recall_batch_cnn_update, precision_batch_cnn_update, accuracy_batch_cnn_update,
+                                  precision_thresh_cnn_update, recall_thresh_cnn_update)
         # loss = 1e4 * tf.reduce_mean(tf.abs(tf.subtract(labels, model_out)))
         # loss_cnn = 1e4 * tf.reduce_mean(tf.abs(tf.subtract(labels, cnn_preds)))
 
@@ -318,6 +350,10 @@ class LSTMValidation(QObject):
         tf.summary.scalar('losses/accuracy_batch', accuracy_batch)
         tf.summary.scalar('losses/precision_batch', precision_batch)
         tf.summary.scalar('losses/recall_batch', recall_batch)
+        [tf.summary.scalar('thresh/precision_' + str(i / 20), precision_thresh[i]) for i in range(20)]
+        [tf.summary.scalar('thresh/recall_' + str(i / 20), recall_thresh[i]) for i in range(20)]
+        # tf.summary.scalar('thresh/precision_thresh', precision_thresh)
+        # tf.summary.scalar('thresh/recall_thresh', recall_thresh)
 
         tf.summary.scalar('losses/Absolute_L1_Loss_CNN', loss_cnn)
         tf.summary.scalar('losses/accuracy_streaming_cnn', accuracy_streaming_cnn)
@@ -326,6 +362,10 @@ class LSTMValidation(QObject):
         tf.summary.scalar('losses/accuracy_batch_cnn', accuracy_batch_cnn)
         tf.summary.scalar('losses/precision_batch_cnn', precision_batch_cnn)
         tf.summary.scalar('losses/recall_batch_cnn', recall_batch_cnn)
+        [tf.summary.scalar('thresh/precision_cnn_' + str(i / 20), precision_thresh_cnn[i]) for i in range(20)]
+        [tf.summary.scalar('thresh/recall_cnn_' + str(i / 20), recall_thresh_cnn[i]) for i in range(20)]
+        # tf.summary.histogram('thresh/precision_thresh_cnn', precision_thresh_cnn)
+        # tf.summary.histogram('thresh/recall_thresh_cnn', recall_thresh_cnn)
 
         my_summary_op = tf.summary.merge_all()
 
@@ -340,14 +380,14 @@ class LSTMValidation(QObject):
 
         logging.info("now starting session")
         # Run the managed session
-        with sv.managed_session() as sess:
+        with sv.managed_session(config=tf.ConfigProto(gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.40))) as sess:
             logging.info("initialiser run")
             for step in range(int(num_steps_per_epoch)):
                 batch_x, batch_y, cnn_y, cnn_logits = self.dataloader.next_batch()
 
                 # Log the summaries every 10 step.
-                loss_value, loss_cnn_value, labels_flat_value, summaries,\
-                global_step_count, _1, _2, acc_value, acc_value_cnn = sess.run([loss, loss_cnn, labels_flat, my_summary_op,
+                loss_value, loss_cnn_value, model_out_flat_value, summaries,\
+                global_step_count, _1, _2, acc_value, acc_value_cnn = sess.run([loss, loss_cnn, model_out_flat, my_summary_op,
                             sv.global_step, metrics_op, metrics_op_cnn, accuracy_batch, accuracy_batch_cnn],
                             feed_dict={images: batch_x, labels: batch_y, cnn_preds: cnn_logits})
                 sv.summary_computed(sess, summaries, global_step=step)
@@ -355,7 +395,7 @@ class LSTMValidation(QObject):
                 logging.info("At step %d/%d, loss= %.4f, accuracy=%.2f; cnn_only_loss= %.4f, cnn_only_accuracy=%.2f",
                              step, int(num_steps_per_epoch * LSTMValidConfig.num_epochs),
                              loss_value, 100*acc_value, loss_cnn_value, 100*acc_value_cnn)
-                self.dataloader.save_predictions(labels_flat_value)
+                self.dataloader.save_predictions(model_out_flat_value)
             logging.info('Finished validation! Saving model to disk now.')
             self.finished.emit()
 
